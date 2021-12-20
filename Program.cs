7ae95d8fadb488b36e2b96f62c0b049eb16d2ce0
cr-ofrisela <ofri.sela@cybereason.com>
2021-12-20 14:04:57 +0200

    Add project files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+using Microsoft.Win32;
+
+string keyAsDateTime = DateTime.Now.ToString("dd_MM_yyyy__HH_mm_ss");
+string executableFile = Environment.GetCommandLineArgs()[0];//System.Reflection.Assembly.GetExecutingAssembly().Location;
+Console.WriteLine("Setting registry value:\r\nKey=HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Run\\" + keyAsDateTime + "\r\nValue=" + executableFile);
+RegistryKey key = Registry.CurrentUser.CreateSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run");
+key.SetValue(keyAsDateTime, executableFile);
+key.Close();
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
